8f9bb49bd1a0f5678743ca8440b09c66a13d8f3f
NghiaLT51 <ltnghia231099@gmail.com>
2026-04-01 16:16:43 +0700

    update build bat

--- build_and_run.bat
+++ build_and_run.bat
@@ -1,32 +1,42 @@
-
-
 @echo off
 REM Build and run both SenderApp and ReceiverApp
 
-set "CMAKE_PREFIX_PATH=C:\Qt\6.10.2\mingw_64\lib\cmake"
+REM Set Qt installation path
+set "QT_PATH=E:\Qt\6.11.0\mingw_64"
+
+REM Set CMake prefix path
+set "CMAKE_PREFIX_PATH=%QT_PATH%\lib\cmake"
 
 REM Set build directory
 set BUILD_DIR=build
 
 REM Create build directory if not exists
 if not exist %BUILD_DIR% mkdir %BUILD_DIR%
+
 cd %BUILD_DIR%
 
+if exist CMakeCache.txt del /f /q CMakeCache.txt
+if exist CMakeFiles rmdir /s /q CMakeFiles
+
 REM Run CMake configure and build
-cmake ..
+cmake .. -G "MinGW Makefiles"
 if errorlevel 1 goto error
 cmake --build .
 if errorlevel 1 goto error
 
+if not exist ReceiverApp_2\ReceiverApp_2.exe (
+    echo ReceiverApp_2.exe not found!
+    goto error
+)
 
 REM Deploy Qt DLLs and QML for SenderApp
-"C:\Qt\6.10.2\mingw_64\bin\windeployqt.exe" SenderApp\SenderApp.exe
+"%QT_PATH%\bin\windeployqt.exe" SenderApp\SenderApp.exe
 
 REM Deploy Qt DLLs and QML for ReceiverApp
-"C:\Qt\6.10.2\mingw_64\bin\windeployqt.exe" ReceiverApp\ReceiverApp.exe
+"%QT_PATH%\bin\windeployqt.exe" ReceiverApp\ReceiverApp.exe
 
 REM Deploy Qt DLLs and QML for ReceiverApp_2
-"C:\Qt\6.10.2\mingw_64\bin\windeployqt.exe" ReceiverApp_2\ReceiverApp_2.exe
+"%QT_PATH%\bin\windeployqt.exe" ReceiverApp_2\ReceiverApp_2.exe
 
 REM Run both apps (in background)
 
